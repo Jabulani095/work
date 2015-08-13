e7907c1a4fa38a9a2e5618abc0afafe374becd4d
Jabulani095 <jaybeedzivas@gmail.com>
2015-08-13 15:04:10 +0200

    rel

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'users#index'
+  get 'microposts/index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
